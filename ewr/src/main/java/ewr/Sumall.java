c0ef284b1a12efa0a9245f3dbff98348b6edaa08
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-04-28 16:54:36 +0530

    vc bcdvb

---
+++ ewr/src/main/java/ewr/Sumall.java
@@ -0,0 +1,22 @@
+package ewr;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Sumall {
+	static Logger logger=Logger.getLogger(Sumall.class);
+
+	public static void main(String[] args) {
+String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message...:"+str);
+		logger.error("error message...:"+str);
+
+	}
+
+}
