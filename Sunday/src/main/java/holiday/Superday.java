a2564d41f1dab4127532cf41e90488ae725f2629
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-05-03 10:09:52 +0530

    new

---
+++ Sunday/src/main/java/holiday/Superday.java
@@ -0,0 +1,24 @@
+package holiday;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+
+public class Superday {
+	static Logger logger=Logger.getLogger(Superday.class);
+
+	public static void main(String[] args) {
+		String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message...:"+str);
+		logger.error("error message....:"+str);
+
+	}
+
+}
