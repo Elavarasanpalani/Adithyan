65212e68eedf05e48b3b8ddaea97a40a3ff196e0
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-05-03 10:22:55 +0530

    yerdtrfc

---
+++ Sun/src/main/java/moon/Mass.java
@@ -0,0 +1,23 @@
+package moon;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Mass {
+	static Logger logger=Logger.getLogger(Mass.class);
+
+	public static void main(String[] args) {
+String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message..ufyfk..:"+str);
+		logger.warn("warning message..yiuguiyg.:"+str);
+		logger.error("error messagejhhgvjh......:"+str);
+		
+
+	}
+
+}
