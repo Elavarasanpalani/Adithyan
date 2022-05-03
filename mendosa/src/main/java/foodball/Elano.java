22da2c50d1cf73243565874e6479996fed6854bf
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-05-03 10:26:38 +0530

    yhdfygj

---
+++ mendosa/src/main/java/foodball/Elano.java
@@ -0,0 +1,22 @@
+package foodball;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Elano {
+	static Logger logger=Logger.getLogger(Elano.class);
+
+	public static void main(String[] args) {
+		String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message..dfyrt.:"+str);
+		logger.error("error message....ftuyrtu......:"+str);
+
+	}
+
+}
