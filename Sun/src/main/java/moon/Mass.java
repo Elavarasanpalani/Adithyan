c2ecddab640fc2f2a295f295fc03bd9dfca19b51
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-05-04 14:13:04 +0530

    uhfuthydkt

--- Sun/src/main/java/moon/Mass.java
+++ Sun/src/main/java/moon/Mass.java
@@ -1,23 +1,14 @@
 package moon;
 
-import java.io.File;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import java.util.ArrayList;
 
 public class Mass {
-	static Logger logger=Logger.getLogger(Mass.class);
-
-	public static void main(String[] args) {
-String str="welcome to sonar";
-		
-		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
-		PropertyConfigurator.configure(log4jConfigFile);
-		logger.info("this is a message..ufyfk..:"+str);
-		logger.warn("warning message..yiuguiyg.:"+str);
-		logger.error("error messagejhhgvjh....urtfuxf..:"+str);
-		
-
-	}
-
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
 }
