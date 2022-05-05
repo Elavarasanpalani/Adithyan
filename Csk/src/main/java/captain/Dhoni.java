6ee354614fd21ef206c55af1e0a1e85f9b8644ee
adithyan.c <adithyan.c@DT150142.QICHOAPPL.com>
2022-05-05 14:41:55 +0530

    new msg

---
+++ Csk/src/main/java/captain/Dhoni.java
@@ -0,0 +1,26 @@
+package captain;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+
+public class Dhoni {
+	static Logger logger=Logger.getLogger(Dhoni.class);
+
+	public static void main(String[] args) {
+String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message...:"+str);
+		logger.error("error message.....:"+str);
+
+	
+
+	}
+
+}
